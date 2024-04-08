182f9e98140f05be20ef5d93c72e2395d43ec639
ryukyi <ryukyi@example.com>
2024-04-08 15:54:24 +1000

    feat: gcp managed cluster create and delete

--- 03_install/setup-worker.sh
+++ 03_install/setup-worker.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 # $worker_external_ip=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
-
